69023639d29412d8b328ae6adf8dcf01e7b90f0f
Iman Tabrizian <itabrizian@nvidia.com>
2020-07-30 12:44:53 -0400

    fix

--- src/clients/c++/library/common.h
+++ src/clients/c++/library/common.h
@@ -40,10 +40,6 @@
 #include <thread>
 #include <vector>
 
-constexpr char kInferHeaderContentLengthHTTPHeader[] =
-    "Inference-Header-Content-Length";
-constexpr int MAX_GRPC_MESSAGE_SIZE = INT32_MAX;
-
 namespace nvidia { namespace inferenceserver { namespace client {
 
 constexpr char kInferHeaderContentLengthHTTPHeader[] =
